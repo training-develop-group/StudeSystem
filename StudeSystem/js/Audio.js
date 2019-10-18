3b413f6c0158c17ab2f14893ce6aac8e738b909b
hewenwu <1960143587@qq.com>
2019-10-18 10:02:07 +0800

    学习系统

---
+++ StudeSystem/js/Audio.js
@@ -0,0 +1,17 @@
+$(function() {
+
+	$(".search").focus(function() {
+		$('.searchIcon').hide();
+	});
+	$(".search").blur(function() {
+		if ($('.search').val() == '')
+			$('.searchIcon').show();
+	});
+	 layui.use(['layer', 'form'], function () {
+	        var layer = layui.layer, form = layui.form;
+	
+	        All.getMenu({
+	            num:2
+	        });
+	    });
+});
