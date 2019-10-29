d87a838ae4c3a4cebf1d4bb8fe5a60052ea0a735
mc <3308653556@qq.com>
2019-10-29 13:11:34 +0800

    Squashed commit of the following:
    commit 995f1ffc8124753ca4100d493fe37bb00b782923
    Author: wangtianque <992270197@qq.com>
    Date:   Tue Oct 29 13:03:36 2019 +0800
    
        2019.10.29
    
    commit 0a5c6bee0deea01ee518fd03e91766a16a25e3f5
    Author: wangtianque <992270197@qq.com>
    Date:   Tue Oct 29 12:52:37 2019 +0800
    
        2019.10.29
    
        Signed-off-by: wangtianque <992270197@qq.com>
    
    commit 78cac74677b829cb3963ae48ee59e905c271620c
    Author: Misaki <1533751317@qq.com>
    Date:   Tue Oct 29 11:10:44 2019 +0800
    
        公共修改
    
    # Conflicts:
    #	HomePage/HomePage.html
    #	TestQuestions/TestQuestions.html
    #	TestQuestions/css/TestQuestions.css
    #	TestQuestions/js/TestQuestions.js
    #	js/common.js

---
+++ js/common.js
@@ -0,0 +1,143 @@
+/**
+ * @name: common
+ * @author：
+ */
+$(function() {
+	layui.use(['layer', 'form'], function() {
+		var layer = layui.layer,
+			form = layui.form;
+
+	});
+
+	All.getMenu();
+});
+
+var All = {
+	getMenu: function(options) {
+		if (options && options != null) {
+			var navHtml = [];
+			navHtml.push('<div class="headerBox">')
+			navHtml.push('<div class="logobox">')
+			navHtml.push('<span>Logo</span>')
+			navHtml.push('</div>')
+			navHtml.push('<div class="navbox">')
+			navHtml.push('<ul>')
+			switch (options.num) {
+				case 1:
+					navHtml.push('<li class="active">首页</li>')
+					navHtml.push('<li class="resource">资源</li>')
+					navHtml.push('<li class="taskPage">任务</li>')
+					navHtml.push('<li class="EditorTestPaper">试卷</li>')
+					navHtml.push('<li class="testQuestions">试题</li>')
+					navHtml.push('<li>统计</li>')
+					// navHtml.push('<li>用户管理</li>')
+					break;
+				case 2:
+					navHtml.push('<li class="homePage">首页</li>')
+					navHtml.push('<li class="active">资源</li>')
+					navHtml.push('<li class="taskPage">任务</li>')
+					navHtml.push('<li class="EditorTestPaper">试卷</li>')
+					navHtml.push('<li class="testQuestions">试题</li>')
+					navHtml.push('<li>统计</li>')
+					// navHtml.push('<li>用户管理</li>')
+					break;
+				case 3:
+					navHtml.push('<li class="homePage">首页</li>')
+					navHtml.push('<li class="resource">资源</li>')
+					navHtml.push('<li class="active">任务</li>')
+					navHtml.push('<li class="EditorTestPaper">试卷</li>')
+					navHtml.push('<li class="testQuestions">试题</li>')
+					navHtml.push('<li>统计</li>')
+					// navHtml.push('<li>用户管理</li>')
+					break;
+				case 4:
+					navHtml.push('<li class="homePage" id="homePage">首页</li>')
+					navHtml.push('<li class="resource" id="resource">资源</li>')
+					navHtml.push('<li class="taskPage" id="taskPage">任务</li>')
+					navHtml.push('<li class="active">试卷</li>')
+					navHtml.push('<li class="testQuestions" id="testQuestions">试题</li>')
+					navHtml.push('<li>统计</li>')
+					// navHtml.push('<li>用户管理</li>')
+					break;
+				case 5:
+					navHtml.push('<li class="homePage">首页</li>')
+					navHtml.push('<li class="resource">资源</li>')
+					navHtml.push('<li class="taskPage">任务</li>')
+					navHtml.push('<li class="EditorTestPaper">试卷</li>')
+					navHtml.push('<li class="active">试题</li>')
+					navHtml.push('<li>统计</li>')
+					// navHtml.push('<li>用户管理</li>')
+					break;
+				case 6:
+					navHtml.push('<li>首页</li>')
+					navHtml.push('<li>资源</li>')
+					navHtml.push('<li>任务</li>')
+					navHtml.push('<li>试卷</li>')
+					navHtml.push('<li>试题</li>')
+					navHtml.push('<li class="active">统计</li>')
+					// navHtml.push('<li>用户管理</li>')
+					break;
+				case 7:
+					navHtml.push('<li>首页</li>')
+					navHtml.push('<li>资源</li>')
+					navHtml.push('<li>任务</li>')
+					navHtml.push('<li>试卷</li>');
+					navHtml.push('<li>试题</li>');
+					navHtml.push('<li>统计</li>');
+					// navHtml.push('<li class="active">用户管理</li>')
+					break;
+			}
+			navHtml.push('</ul>');
+			navHtml.push('</div>');
+			navHtml.push('<div class="right">');
+			navHtml.push('<img src="../imgs/HeadPortrait.jpg" class="userimg">');
+			navHtml.push('<span class="username">用户名</span>');
+			navHtml.push('<img src="./../imgs/et.png" class="logout">');
+			navHtml.push('</div>');
+			navHtml.push('<div class="left">');
+			navHtml.push('<div class="searchbox">');
+			navHtml.push('<img src="./../imgs/search.jpg" class="iconSearch">');
+			navHtml.push('<input type="text" class="search" name="" value="" />');
+			navHtml.push('</div>');
+			navHtml.push('</div>');
+			navHtml.push('</div>');
+			
+			$('#header').html(navHtml.join(''));
+			$('.EditorTestPaper').click(function() {
+				console.log(1);
+				// 跳转到试题
+				window.location.href = "../ExaminationPaperPage/ExaminationPaperPage.html";
+			});
+			
+			$('.resource').click(function(){
+				window.location.href = "../ResourcePage/ResourcePage.html";
+			})
+
+			$('.testQuestions').click(function() {
+				console.log(1);
+				// 跳转到试题
+				window.location.href = "../TestQuestions/TestQuestions.html";
+			});
+			$('.taskPage').click(function(){
+				window.location.href = "../TaskPage/TaskPage.html";
+			})
+
+			$('.homePage').click(function(){
+				window.location.href = "../HomePage/HomePage.html";
+			})
+
+
+			// //点击头像显示推出登陆
+			// $('body').off('click').on('click',function () {
+			// 	$('.logout').slideUp('slow');
+			// });
+			//
+			// $('.userimg').off('click').on('click',function (event) {
+			// 	event.stopPropagation();
+			// 	$('.logout').slideToggle('slow');
+			//
+			// })
+
+		}
+	}
+}
