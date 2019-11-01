cc1b768e02f1627763adcc5639f53a37e289fd38
Misaki <1533751317@qq.com>
2019-11-01 14:54:15 +0800

    Edit login page

--- js/LogIn.js
+++ js/LogIn.js
@@ -1,28 +1,31 @@
-$(function() {
-    $('.logIn').off('click').on('click', function() {
-    	var data = {
-    		userName: $('#user').val(),
-    		password: $('#pwd').val()
-    	};
-    	if (data.userName == '' || data.password == '') {
-    		layer.msg('用户名或者密码不能为空');
-    		return;
-    	}
-    	// $.ajax({
-    	//     url: loginUrl + 'manage_system/login',
-    	//     data:data,
-    	//     dataType:'json',
-    	//     type: "get",
-    	//     success: function (res) {
-    	// if(res.code == 1){
-    	window.location.href = 'HomePage/HomePage.html'
-    	// }else {
-    	//     layer.msg(res.msg);
-    	// }
-    	//     },
-    	//     error: function (e) {
-    	//
-    	//     }
-    	// })
+$(function () {
+    $('.logIn').off('click').on('click', function () {
+        var data = {
+            userName: $('#user').val(),
+            password: $('#pwd').val()
+        };
+        if (data.userName == '' || data.password == '') {
+            layer.msg('用户名或者密码不能为空');
+            return;
+        }
+        $.ajax({
+            url: loginUrl + 'manage_system/login',
+            data: data,
+            dataType: 'json',
+            type: "get",
+            success: function (res) {
+                if (res.code == 1) {
+                    window.location.href = 'HomePage/HomePage.html'
+                } else {
+                    layer.msg(res.msg);
+                }
+
+                // todo 如果密码错误，$('.errorMsg p').text('*密码错误')
+
+            },
+            error: function (e) {
+
+            }
+        })
     })
 });
