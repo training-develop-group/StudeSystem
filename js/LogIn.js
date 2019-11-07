1d1cae9724e57b968d595347dd7e342591808cb1
LB-role <1784659981@qq.com>
2019-11-07 16:58:53 +0800

    2019年11月7日16:58:50

--- js/LogIn.js
+++ js/LogIn.js
@@ -1,28 +1,48 @@
 $(function() {
-    $('.logIn').off('click').on('click', function() {
-    	var data = {
-    		userName: $('#user').val(),
-    		password: $('#pwd').val()
-    	};
-    	if (data.userName == '' || data.password == '') {
-    		layer.msg('用户名或者密码不能为空');
-    		return;
-    	}
-    	// $.ajax({
-    	//     url: loginUrl + 'manage_system/login',
-    	//     data:data,
-    	//     dataType:'json',
-    	//     type: "get",
-    	//     success: function (res) {
-    	// if(res.code == 1){
-    	window.location.href = 'HomePage/HomePage.html'
-    	// }else {
-    	//     layer.msg(res.msg);
-    	// }
-    	//     },
-    	//     error: function (e) {
-    	//
-    	//     }
-    	// })
-    })
+	$('.logIn').off('click').on('click', function() {
+		var data = {
+			userName: $('#user').val(),
+			password: $('#pwd').val()
+		};
+		if (data.userName == '' || data.password == '') {
+			layer.msg('用户名或者密码不能为空');
+			return false;
+		}
+		$.ajax({
+			url: LBUrl + 'manage_system/login',
+			data:data,
+			dataType:'json',
+			type: "GET",
+			success: function (res) {
+				console.log(res);
+				if(res.code == 1){
+					window.location.href = 'HomePage/HomePage.html'
+					console.log(res.msg);
+				}else {
+					layer.msg(res.msg);
+				}
+			},
+			error: function (e) {
+		
+			}
+		})
+	// 	$.ajax({
+	// 		url: LBUrl + 'manage_system/login/user',
+	// 		data:{},
+	// 		dataType:'json',
+	// 		type: "GET",
+	// 		success: function (res) {
+	// 			console.log(res);
+	// 			if(res.code == 1){
+	// 				// window.location.href = 'HomePage/HomePage.html'
+	// 				console.log(res);
+	// 			}else {
+	// 				layer.msg(res.msg);
+	// 			}
+	// 		},
+	// 		error: function (e) {
+		
+	// 		}
+	// 	})
+	})
 });
