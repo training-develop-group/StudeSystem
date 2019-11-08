46e9c7e95e43d4f2794314da7c35f007030dec19
Misaki <1533751317@qq.com>
2019-11-08 11:07:31 +0800

    学生首页

--- UserHomePage/js/UserHomePage.js
+++ UserHomePage/js/UserHomePage.js
@@ -53,42 +53,42 @@ var info = {
         //     success:function (res) {
         //         // 请求成功时
 
-                var time = new Date();
-                var taskList = [
-                    {taskName: '这里11111', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'1',taskType:'1'},
-                    {taskName: '测试任务2', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'2',taskType:'2'},
-                    {taskName: '测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'3',taskType:'3'},
-                    {taskName: '测试任务4', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'4',taskType:'2'},
-                    {taskName: '测试任务5', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'5',taskType:'3'},
-                    {taskName: '测试任务6', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'6',taskType:'1'}
-                ];
+        var time = new Date();
+        var taskList = [
+            {taskName: '这里11111', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'1',taskType:'1'},
+            {taskName: '测试任务2', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'2',taskType:'2'},
+            {taskName: '测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3测试任务3', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'3',taskType:'3'},
+            {taskName: '测试任务4', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'4',taskType:'2'},
+            {taskName: '测试任务5', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'5',taskType:'3'},
+            {taskName: '测试任务6', taskTime: time, taskImg: '../imgs/HeadPortrait.jpg',taskId:'6',taskType:'1'}
+        ];
 
-                if (taskList) {
-                    var listHtml = [];
-                    taskList.forEach(function (item, index) {
-                        listHtml.push('<li class="clearfix">');
-                        listHtml.push('   <div class="leftBox">');
-                        listHtml.push('      <div class="imgBox ">');
-                        listHtml.push('          <img src="' + item.taskImg + '" alt="任务图片"/>');
-                        listHtml.push('      </div>');
-                        listHtml.push('   </div>');
-                        listHtml.push('   <div class="rightBox">');
-                        if(item.taskName.length > 50){
-                            listHtml.push('       <h3><b title="'+ item.taskName + '" class="ToTask" data-id="'+ item.taskId +'" data-type="'+ item.taskType+'">'+ item.taskName.substring(0, 60) + '...</b></h3>');
-                        } else {
-                            listHtml.push('       <h3><b title="'+ item.taskName + '" class="ToTask" data-id="'+ item.taskId +'" data-type="'+ item.taskType+'">'+ item.taskName + '</b></h3>');
-                        }
-                        listHtml.push('       <p>'+ item.taskTime +'</p>');
-                        listHtml.push('   </div>');
-                        listHtml.push('</li>');
-                    });
-
-                    $('#taskList').html(listHtml.join(''));
-                    //分页方法
-                    info.Page(total, curr);
-                    // 任务点击跳转任务详情
-                    info.ToTask();
+        if (taskList) {
+            var listHtml = [];
+            taskList.forEach(function (item, index) {
+                listHtml.push('<li class="clearfix">');
+                listHtml.push('   <div class="leftBox">');
+                listHtml.push('      <div class="imgBox ">');
+                listHtml.push('          <img src="' + item.taskImg + '" alt="任务图片"/>');
+                listHtml.push('      </div>');
+                listHtml.push('   </div>');
+                listHtml.push('   <div class="rightBox">');
+                if(item.taskName.length > 50){
+                    listHtml.push('       <h3><b title="'+ item.taskName + '" class="ToTask" data-id="'+ item.taskId +'" data-type="'+ item.taskType+'">'+ item.taskName.substring(0, 60) + '...</b></h3>');
+                } else {
+                    listHtml.push('       <h3><b title="'+ item.taskName + '" class="ToTask" data-id="'+ item.taskId +'" data-type="'+ item.taskType+'">'+ item.taskName + '</b></h3>');
                 }
+                listHtml.push('       <p>'+ item.taskTime +'</p>');
+                listHtml.push('   </div>');
+                listHtml.push('</li>');
+            });
+
+            $('#taskList').html(listHtml.join(''));
+            //分页方法
+            info.Page(total, curr);
+            // 任务点击跳转
+            info.ToTask();
+        }
         //     },
         //     error:function (e) {
         //         // 请求失败时
