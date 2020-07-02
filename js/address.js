f09bfd649ed89f20c41a675bda99f8a735999ddb
HSCarlson <hs_carlson@163.com>
2020-07-02 18:47:01 +0800

    前台整合

--- js/address.js
+++ js/address.js
@@ -7,6 +7,6 @@
 Url = 'http://localhost:8080/res-service/';
 FileUrl = 'http://localhost:8080/res-service/manage_system/file/';
 LoginUrl ='http://localhost:8080/auth-service/'
-
+Upload = 'http://localhost:8080/zuul/res-service/'
 // 服务器
 // Url = 'http://dlyunyi.f3322.net:8888/res-service/';
