d9fd5867c3ae58fc6e6c238aed8198c205047d66
HSCarlson <hs_carlson@163.com>
2020-07-06 09:22:50 +0800

    丁浩然　服务器地址

--- js/address.js
+++ js/address.js
@@ -9,10 +9,7 @@ FileUrl = 'http://localhost:8080/res-service/manage_system/file/';
 LoginUrl ='http://localhost:8080/auth-service/';
 Upload = 'http://localhost:8080/zuul/res-service/';
 // 服务器
-// Url = 'http://dlyunyi.f3322.net:8082/res-service/';
-netUrl = {
-    "Url" :'',
-    "FileUrl" :'',
-    "Upload":'',
-    "LoginUrl":''
-}
+// Url = 'http://dlyunyi.f3322.net:8080/res-service/';
+// FileUrl = 'http://dlyunyi.f3322.net:8080/res-service/manage_system/file/';
+//LoginUrl ='http://dlyunyi.f3322.net:8080/auth-service/';
+// Upload = 'http://dlyunyi.f3322.net:8080/zuul/res-service/';
