59f9bffd0eeabde231f47df598bd465a8c25d13c
HSCarlson <hs_carlson@163.com>
2020-07-09 19:53:19 +0800

    丁浩然 url更新

--- js/address.js
+++ js/address.js
@@ -4,12 +4,12 @@
  
 
 // 本地
-Url = 'http://localhost:8080/res-service/';
-FileUrl = 'http://localhost:8080/res-service/manage_system/file/';
-LoginUrl ='http://localhost:8080/auth-service/';
-Upload = 'http://localhost:8080/zuul/res-service/';
+// Url = 'http://localhost:8080/res-service/';
+// FileUrl = 'http://localhost:8080/res-service/manage_system/file/';
+// LoginUrl ='http://localhost:8080/auth-service/';
+// Upload = 'http://localhost:8080/zuul/res-service/';
 // 服务器
-// Url = 'http://dlyunyi.f3322.net:8080/res-service/';
-// FileUrl = 'http://dlyunyi.f3322.net:8080/res-service/manage_system/file/';
-//LoginUrl ='http://dlyunyi.f3322.net:8080/auth-service/';
-// Upload = 'http://dlyunyi.f3322.net:8080/zuul/res-service/';
+Url = 'http://dlyunyi.f3322.net:8080/res-service/';
+FileUrl = 'http://dlyunyi.f3322.net:8080/res-service/manage_system/file/';
+LoginUrl ='http://dlyunyi.f3322.net:8080/auth-service/';
+Upload = 'http://dlyunyi.f3322.net:8080/zuul/res-service/';
